39c967c1dbc71d84aa6b2fef47dec6eaa6ad342e
Toonu <Toonu44@gmail.com>
2020-10-31 20:51:16 +0000

    Improved trade mechanics.

--- commands/trade.js
+++ commands/trade.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'trade',
     description: 'Command for setting trade value of assets you have traded! When you purchase or sell an asset, you should put the price you paid here.',
     args: true,
-    usage: '<sell/buy> <money> <assetType> <numberOfAssets> <@customer>\n\n**Assets:**\n*Buildings:* AIRPORT, FOB, PORT, RADAR\n*Surface assets:* MBT, AFV, IFV, APC, SAM, SPAAG, SF\n*Aerospace assets:* L, M, H, LA, VL, VTOL, SAT, OV\n*Naval assets:* K, F, DD, CC, BC, BB, CL, CV\n**Weapons:**\n*Aerial:* SRAAM, MRAAM, LRAAM, AGM, ASHM, ATGM, SRSAM ,MRSAM, LRSAM, SEAD\n*Surface:* CRUISE, BALLISTIC, ABM, ASM\n*Bombs:* UNGUI, GUI, EW, RECON, FUEL, GUNPOD',
+    usage: '<sell/buy> <numberOfAssets> <assetType> <money> <@customer>\n\n**Assets:**\n*Buildings:* AIRPORT, FOB, PORT, RADAR\n*Surface assets:* MBT, AFV, IFV, APC, SAM, SPAAG, SF\n*Aerospace assets:* L, M, H, LA, VL, VTOL, SAT, OV\n*Naval assets:* K, F, DD, CC, BC, BB, CL, CV\n**Weapons:**\n*Aerial:* SRAAM, MRAAM, LRAAM, AGM, ASHM, ATGM, SRSAM ,MRSAM, LRSAM, SEAD\n*Surface:* CRUISE, BALLISTIC, ABM, ASM\n*Bombs:* UNGUI, GUI, EW, RECON, FUEL, GUNPOD',
     cooldown: 5,
     guildOnly: true,
     execute: async function execute(message, args) { 
@@ -13,14 +13,14 @@ module.exports = {
         const units = require('./../units.json');
 
         try {
-            args[1] = parseInt(args[1]);
-            args[3] = parseInt(args[3]);
-            let nation = cfg.users[message.author.id].nation;
-            let type = args[0].toLowerCase();
-            let unit = args[2].toUpperCase();
+            var type = args[0].toLowerCase();
+            var amount = parseInt(args[1]);
+            var unit = args[2].toUpperCase();
+            var money = parseInt(args[3]);
+            var nation = cfg.users[message.author.id].nation;            
 
-            if(!units.hasOwnProperty(unit)) throw 'AssetType not found. Please retry.'
-            if (isNaN(args[0]) || isNaN(args[1])) throw 'Argument money or number of assets is not a number. Canceling operation.';
+            if(!units.hasOwnProperty(unit)) throw 'AssetType not found. Please retry.';
+            if (isNaN(amount) || isNaN(money)) throw 'Argument money or number of assets is not a number. Canceling operation.';
             if (!type.startsWith('sell') && !type.startsWith('buy')) throw 'First argument is not sell or buy.';
             if (nation == undefined) throw 'Nation does not exist in our database. Contact moderator or retry.';
         } catch(err) {
@@ -32,28 +32,38 @@ module.exports = {
         .then(data => {
             if (data[0] == false) {
                 data[0] = 0;
-            } else {
-                data[0] = parseInt(data[0].replace(/[,|$]/g, ''));
             }
             fn.ss(['get', `${fn.toCoord(data[1])+(data[2])}`], message)
-                .then(amount => {
-                    amount = parseInt(amount);
-                    fn.ss(['set', `${fn.toCoord(data[1])+(data[2])}`, data[0] + amount], message)
+                .then(amountOfUnit => {
+                    amountOfUnit = parseInt(amountOfUnit);
+                    var newAmount;
+                    if (type === 'sell') {
+                        newAmount = amountOfUnit - amount;
+                    } else {
+                        newAmount = amountOfUnit + amount;
+                    }
+                    fn.ss(['set', `${fn.toCoord(data[1])+(data[2])}`, newAmount], message)
                         .then(result => {
                             gm.findUnitPrice(type, message, nation)
-                            .then(trade => {
-                                if (trade[0] == false) {
-                                    trade[0] = 0;
-                                } else {
-                                    trade[0] = parseInt(trade[0].replace(/[,|$]/g, ''));
-                                }
-                                fn.ss(['set', `${fn.toCoord(trade[1])+(trade[2])}`, trade[0] + amount], message)
-                                .then(fin => {
-                                    gm.report(message, `<@${message.author.id}> has traded ${args[3]} of ${unit} with ${message.mentions.users.first().id} for ${args[1]}!`);
+                                .then(trade => {
+                                    console.log(trade);
+                                    if (trade[0] == false) {
+                                        trade[0] = 0;
+                                    } else {
+                                        trade[0] = parseInt(trade[0].replace(/[,|$]/g, ''));
+                                    }
+                                    console.log(trade);
+                                    fn.ss(['set', `${fn.toCoord(trade[1])+(trade[2])}`, trade[0] + money], message)
+                                        .then(fin => {
+                                            gm.report(message, `<@${message.author.id}> has traded ${amount} ${unit}s with <@${message.mentions.users.first().id}> for ${money+cfg.money}!`);
+                                        })
+                                        .catch(err => console.error(err));
                                 })
-                            })
-                        }).catch(err => console.error(err));
-                }).catch(err => console.error(err));
+                                .catch(err => console.error(err));
+                        })
+                        .catch(err => console.error(err));
+                })
+                .catch(err => console.error(err));
         })
         .catch(err => console.error(err));    
     },

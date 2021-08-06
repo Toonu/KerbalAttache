d1785e5d2ec1cdf2f1f56bd0984cd690ac8a46d1
Toonu <Toonu44@gmail.com>
2021-08-06 13:44:57 +0000

    Server changes

--- commands/turn.js
+++ commands/turn.js
@@ -116,8 +116,7 @@ module.exports = {
         report(message, `Turn has been finished by <@${message.author.id}>.`, this.name);
         let server = cfg.servers[message.guild.id];
         // noinspection JSUnresolvedFunction,JSUnresolvedVariable
-        //message.client.channels.cache.get(server.announcements).send(`<@&${server.headofstate}> Turn ${cfg.turn}
-        // has been finished!`)
-        //    .catch(error => log(error, true));
+        message.client.channels.cache.get(server.announcements).send(`<@&${server.headofstate}> Turn ${cfg.turn} has been finished!`)
+            .catch(error => log(error, true));
     }
 };
